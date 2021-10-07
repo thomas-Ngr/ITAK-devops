be59badd2fcdf63148769624bf6e6801bbabb306
tom ngr <dont@mail.me>
2021-10-07 12:13:16 +0200

    allow to fetch one random user. Create a list containing JSON values

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-let users = require('./data/user.json');
+const users = require('./data/user.json');
+const usersList = Object.values(users)[0];
 
 // simple root request to validate it works
 app.get('/', (req, res) => {
@@ -11,10 +12,16 @@ app.get('/', (req, res) => {
 
 // GET all users data
 app.get('/getAll', (req, res) => {
-    res.send(users)
+    res.json({ users: usersList})
+});
+
+// GET one random user
+app.get('/getOne', (req, res) => {
+    let randomUserIndex = Math.floor(Math.random() * usersList.length)
+    res.send(usersList[randomUserIndex])
 });
 
 // make server listen
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
